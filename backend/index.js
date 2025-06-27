906a0480e6788b60b832fbe011bbe35ffc156b83
Maurice <maurycy095@gmail.com>
2025-06-27 18:41:05 +0200

    Project setup

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Działa' });
+});
+
+app.listen(port, () => {
+  console.log(`Backend działa na porcie ${port}`);
+});
