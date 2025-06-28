2aa578a8691ac4864e32fac7bd226d958342099b
AgnikAm <KingaAleksandraKlima@gmail.com>
2025-06-28 13:54:45 +0200

    Dodanie backendu z rejestracja i logowaniem

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Działa' });
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Backend działa na porcie ${port}`);
